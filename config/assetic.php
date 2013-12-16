19cf7a9ae39da08005fbc451faec53eed5ab7bf7
MK <kohenkatz@gmail.com>
2013-12-16 01:21:01 -0500

    updated path for yui based on default composer install location

--- config/assetic.php
+++ config/assetic.php
@@ -5,7 +5,7 @@ return array(
 		'assetic_base_dir'    => APPPATH.'views',
 		'debug'               => false,
 		'asset_save_dir'      => APPPATH.'/../public',
-		'yui_comrpessor_path' => MODPATH.'assetic/vendor/YuiCompressor/yuicompressor-2.4.6.jar',
+		'yui_comrpessor_path' => DOCROOT.'vendor/packagist/yuicompressor-bin/bin/yuicompressor.jar',
 		'twig_auto_write'     => true,
 	),
 );
