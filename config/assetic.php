4e0da1b5f917519cf50316307d503a1dc0b57d31
MK <kohenkatz@gmail.com>
2013-12-15 23:40:17 -0500

    formatting

--- config/assetic.php
+++ config/assetic.php
@@ -1,18 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-
-
 return array(
-			'default' => array (
-
-
-				'assetic_base_dir' 		=> APPPATH.'views',
-				'debug' 				=> false,
-				'asset_save_dir'		=> APPPATH.'/../public',
-				'yui_comrpessor_path'	=> MODPATH.'assetic/vendor/YuiCompressor/yuicompressor-2.4.6.jar',
-				'twig_auto_write'		=> true,
-			),
-			
+	'default' => array (
+		'assetic_base_dir'    => APPPATH.'views',
+		'debug'               => false,
+		'asset_save_dir'      => APPPATH.'/../public',
+		'yui_comrpessor_path' => MODPATH.'assetic/vendor/YuiCompressor/yuicompressor-2.4.6.jar',
+		'twig_auto_write'     => true,
+	),
 );
-
-
